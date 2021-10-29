0431970cb2f952cb7685f651987faeb4768a5757
shen-ui <peterhuiwang1995@gmail.com>
2021-10-29 00:37:31 -0400

    most stable build

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use cmake::Config;
-//use std::env;
-//use std::path::Path;
+
+fn out_dir(){
+    
+}
 
 fn main() {
     // check OS and select cpp compiler
@@ -11,29 +13,23 @@ fn main() {
     #[cfg(not(target_os = "macos"))]
     let cpp_lib = "stdc++";
 
-    //let dir = env::var("OUT_DIR").unwrap();
-    //let dest_path = Path::new(&dir).join("hello.rs");
-
-    // set config for library
     let dst = Config::new(".")
         .define("COORDGEN_BUILD_TESTS", "OFF")
         .define("COORDGEN_BUILD_EXAMPLE", "OFF")
         .define("COORDGEN_BUILD_SHARED_LIBS", "OFF")
-        .define("COORDGEN_BUILD_DIR", "/lib")
         .define("CMAKE_BUILD_TYPE", "Release")
         .uses_cxx11()
         .build();
-    // creates link to lib
+    // in either of two directories
     println!(
         "cargo:rustc-link-search=native={}",
         dst.join("lib").display()
     );
-    // creates link to lib64
     println!(
         "cargo:rustc-link-search=native={}",
         dst.join("lib64").display()
     );
-
+    //link libraries
     println!("cargo:rustc-link-lib=static=coordgen");
     println!("cargo:rustc-link-lib=static=wrappedcoordgen");
     println!("cargo:rustc-link-lib={}", cpp_lib);
