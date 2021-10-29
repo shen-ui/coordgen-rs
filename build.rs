05ab68725b2fb5bdaa3dd5552c9078a37ca7c24d
shen-ui <peterhuiwang1995@gmail.com>
2021-10-29 14:37:48 -0400

    Finalized build.

--- build.rs
+++ build.rs
@@ -9,24 +9,40 @@ fn main() {
     #[cfg(not(target_os = "macos"))]
     let cpp_lib = "stdc++";
 
+    //let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    //println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lib").display());
+
+
+    // configure and create CMakeFiles
     let dst = Config::new(".")
         .define("COORDGEN_BUILD_TESTS", "OFF")
         .define("COORDGEN_BUILD_EXAMPLE", "OFF")
         .define("COORDGEN_BUILD_SHARED_LIBS", "OFF")
         .define("CMAKE_BUILD_TYPE", "Release")
+        .define("CMAKE_INSTALL_LIBDIR", "lib")
         .uses_cxx11()
         .build();
-    // in either of two directories
+    
+    //need to hardcode make --path="${prefix}/lib"
     println!(
         "cargo:rustc-link-search=native={}",
         dst.join("lib").display()
     );
-    println!(
-        "cargo:rustc-link-search=native={}",
-        dst.join("lib64").display()
-    );
-    //link libraries
+
+    //link libraries to 
     println!("cargo:rustc-link-lib=static=coordgen");
     println!("cargo:rustc-link-lib=static=wrappedcoordgen");
     println!("cargo:rustc-link-lib={}", cpp_lib);
 }
+/*
+        println!(
+            "cargo:rustc-link-search=native={}",
+            dst.join("lib64").display()
+        );*/
+        //.define("CMAKE_LIBRARY_OUTPUT_DIRECTORY", "/lib")
+        //.build_target("target/lib")
+
+        //let dst = cmake::build("./");
+
+            //let path = PathBuf::from("target/lib");
+    //assert_eq!(Path::new("target/lib"), path.as_path());
